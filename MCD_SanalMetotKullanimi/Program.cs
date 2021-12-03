b47e503d3a5a4de80126cdb68f8b21d2c74ee2ec
emre gemici <gmcc.emree@gmail.com>
2021-12-03 17:14:59 +0300

    Add project files.

---
+++ MCD_SanalMetotKullanimi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_SanalMetotKullanimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Televizyon T1 = new Televizyon();
+            T1.ekranaYaz("Hangisi?");
+            Console.WriteLine("******************");
+            Urun U1 = new Urun();
+            U1.ekranaYaz("Merhaba");
+            Console.WriteLine("******************");
+            baseClass B1 = new baseClass();
+            B1.ekranaYaz("Merhaba");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MCD_SanalMetotKullanimi/Urun.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_SanalMetotKullanimi
+{
+    public class Urun:baseClass 
+    {
+        public Urun()
+        {
+            Console.WriteLine("urun");
+        }
+
+        public override void ekranaYaz(string data)
+        {
+            Console.WriteLine("urun => "+ data );
+        }
+    }
+}
